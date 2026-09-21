b34654499f0d48ea5f41ce26ba1b09f0d37c85d7
c-benjamin.nguyen <c-benjamin.nguyen@avepoint.com>
2026-09-21 10:12:03 +0700

    update: add skeleton files

---
+++ BackEnd/src/StudentPortal.Service/Helpers/JwtTokenHelper.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentPortal.Service.Helpers
+{
+    internal class JwtTokenHelper
+    {
+    }
+}
